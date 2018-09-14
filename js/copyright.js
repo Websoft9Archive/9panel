5d740f5e0cf65efa7f778dcc65581b4185297d48
darren chen <chendelin1982@126.com>
2018-09-14 16:31:23 +0800

    12ls

---
+++ js/copyright.js
@@ -0,0 +1,7 @@
+document.writeln("<div class=\'row\'>");
+document.writeln("                            <div class=\'col-md-12\'>");
+document.writeln("                                <div class=\'copyright\'>");
+document.writeln("                                    <p>Copyright © 2018 长沙网久软件有限公司. All rights reserved. 本前端框架由 <a href=\'https://colorlib.com\'>Colorlib</a>提供.</p>");
+document.writeln("                                </div>");
+document.writeln("                            </div>");
+document.writeln("                        </div>");
