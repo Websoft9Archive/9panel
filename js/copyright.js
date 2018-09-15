661b3a31f32c9da9411f7185fe22c7f9db526461
darren chen <chendelin1982@126.com>
2018-09-15 16:07:20 +0800

    cdl

--- js/copyright.js
+++ js/copyright.js
@@ -1,7 +1,7 @@
 document.writeln("<div class=\'row\'>");
 document.writeln("                            <div class=\'col-md-12\'>");
 document.writeln("                                <div class=\'copyright\'>");
-document.writeln("                                    <p>Copyright © 2018 长沙网久软件有限公司. All rights reserved. 本前端框架由 <a href=\'https://github.com/ColorlibHQ/CoolAdmin\' target=\'_blank\'>Colorlib</a>提供.</p>");
+document.writeln("                                    <p>Copyright © 2018 长沙网久软件有限公司. All rights reserved. 本前端框架由 <a href=\'https://github.com/ColorlibHQ/CoolAdmin\' target=\'_blank\'>Colorlib</a>提供. </p>");
 document.writeln("                                </div>");
 document.writeln("                            </div>");
 document.writeln("                        </div>");
